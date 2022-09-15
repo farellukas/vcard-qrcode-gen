95323e966087fe99a5cc0303e87ef2b3f48f2a89
Farel Lukas <farellukas@gmail.com>
2022-09-15 02:07:39 -0600

    Optimized QR code sizes

--- VCardmaker/qr_generator/views.py
+++ VCardmaker/qr_generator/views.py
@@ -30,11 +30,11 @@ def qr(request):
   vcard = vcardgen.create_vcard()
 
   vcardgen.add_vcard_name(vcard, first_name, middle_name, last_name)
-  vcardgen.add_vcard_email(vcard, email)
-  vcardgen.add_vcard_phone(vcard, phone)
-  vcardgen.add_vcard_company(vcard, company, position)
-  vcardgen.add_vcard_address(vcard, street, city, region, postal_code, country)
-  vcardgen.add_vcard_website(vcard, website)
+  if email.strip(): vcardgen.add_vcard_email(vcard, email)
+  if phone.strip(): vcardgen.add_vcard_phone(vcard, phone)
+  if company.strip() or position.strip(): vcardgen.add_vcard_company(vcard, company, position)
+  if street.strip() or city.strip() or region.strip() or postal_code.strip() or country.strip(): vcardgen.add_vcard_address(vcard, street, city, region, postal_code, country)
+  if website.strip(): vcardgen.add_vcard_website(vcard, website)
 
   vcard_output = vcardgen.output_vcard(vcard)
 
