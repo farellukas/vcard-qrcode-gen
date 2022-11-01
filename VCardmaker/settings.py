7b680cfcadb38783bd10041efe892d592c57f314
Farel Lukas <farellukas@gmail.com>
2022-11-01 14:35:17 -0600

    Collected static files

--- VCardmaker/settings.py
+++ VCardmaker/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
